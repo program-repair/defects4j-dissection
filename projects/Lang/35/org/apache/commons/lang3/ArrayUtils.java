147cb901d71d909e558d5d811813e71f1024bcb0
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:26:29 +0100

    fixed files form Lang #35

--- projects/Lang/35/org/apache/commons/lang3/ArrayUtils.java
+++ projects/Lang/35/org/apache/commons/lang3/ArrayUtils.java
@@ -3292,7 +3292,7 @@ public class ArrayUtils {
         } else if (element != null) {
             type = element.getClass();
         } else {
-            type = Object.class;
+            throw new IllegalArgumentException("Arguments cannot both be null");            
         }
         @SuppressWarnings("unchecked") // type must be T
         T[] newArray = (T[]) copyArrayGrow1(array, type);
@@ -3571,7 +3571,7 @@ public class ArrayUtils {
         } else if (element != null) {
             clss = element.getClass();
         } else {
-            return (T[]) new Object[] { null };
+            throw new IllegalArgumentException("Array and element cannot both be null");            
         }
         @SuppressWarnings("unchecked") // the add method creates an array of type clss, which is type T
         final T[] newArray = (T[]) add(array, index, element, clss);
