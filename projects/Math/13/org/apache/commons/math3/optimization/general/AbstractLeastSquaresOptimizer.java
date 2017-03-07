5fef7b8cc1e7c24a6e673005e5c1f57bfedb765b
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:51 +0100

    fixed files form Math #13

--- projects/Math/13/org/apache/commons/math3/optimization/general/AbstractLeastSquaresOptimizer.java
+++ projects/Math/13/org/apache/commons/math3/optimization/general/AbstractLeastSquaresOptimizer.java
@@ -559,7 +559,16 @@ public abstract class AbstractLeastSquaresOptimizer
      * @return the square-root of the weight matrix.
      */
     private RealMatrix squareRoot(RealMatrix m) {
+        if (m instanceof DiagonalMatrix) {
+            final int dim = m.getRowDimension();
+            final RealMatrix sqrtM = new DiagonalMatrix(dim);
+            for (int i = 0; i < dim; i++) {
+               sqrtM.setEntry(i, i, FastMath.sqrt(m.getEntry(i, i)));
+            }
+            return sqrtM;
+        } else {
             final EigenDecomposition dec = new EigenDecomposition(m);
             return dec.getSquareRoot();
+        }
     }
 }
