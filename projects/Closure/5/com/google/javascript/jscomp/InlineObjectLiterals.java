2d5f8783f62677bedd3a7319e861e20820475b73
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:21:02 +0100

    fixed files form Closure #5

--- projects/Closure/5/com/google/javascript/jscomp/InlineObjectLiterals.java
+++ projects/Closure/5/com/google/javascript/jscomp/InlineObjectLiterals.java
@@ -173,6 +173,9 @@ class InlineObjectLiterals implements CompilerPass {
 
           // Deleting a property has different semantics from deleting
           // a variable, so deleted properties should not be inlined.
+          if (gramps.isDelProp()) {
+            return false;
+          }
 
           // NOTE(nicksantos): This pass's object-splitting algorithm has
           // a blind spot. It assumes that if a property isn't defined on an
