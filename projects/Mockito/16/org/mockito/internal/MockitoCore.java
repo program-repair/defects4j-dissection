dd9f3710757cc652f2681fba7ac28e1845e0a31a
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:27:48 +0100

    fixed files form Mockito #16

--- projects/Mockito/16/org/mockito/internal/MockitoCore.java
+++ projects/Mockito/16/org/mockito/internal/MockitoCore.java
@@ -29,10 +29,11 @@ public class MockitoCore {
     private final MockUtil mockUtil = new MockUtil();
     private final MockingProgress mockingProgress = new ThreadSafeMockingProgress();
     
-    public <T> T mock(Class<T> classToMock, MockSettings mockSettings, boolean shouldResetOngoingStubbing) { return mock(classToMock, mockSettings); }
-    public <T> T mock(Class<T> classToMock, MockSettings mockSettings) {
+    public <T> T mock(Class<T> classToMock, MockSettings mockSettings, boolean shouldResetOngoingStubbing) {
         mockingProgress.validateState();
+        if (shouldResetOngoingStubbing) {
             mockingProgress.resetOngoingStubbing();
+        }
         return mockUtil.createMock(classToMock, (MockSettingsImpl) mockSettings);
     }
     
