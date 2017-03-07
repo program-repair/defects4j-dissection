324e93719ff4d81af37f270e199a7d9f7943392e
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:21:23 +0100

    fixed files form Closure #12

--- projects/Closure/12/com/google/javascript/jscomp/MaybeReachingVariableUse.java
+++ projects/Closure/12/com/google/javascript/jscomp/MaybeReachingVariableUse.java
@@ -157,6 +157,12 @@ class MaybeReachingVariableUse extends
   }
 
   private boolean hasExceptionHandler(Node cfgNode) {
+    List<DiGraphEdge<Node, Branch>> branchEdges = getCfg().getOutEdges(cfgNode);
+    for (DiGraphEdge<Node, Branch> edge : branchEdges) {
+      if (edge.getValue() == Branch.ON_EX) {
+        return true;
+      }
+    }
     return false;
   }
 
