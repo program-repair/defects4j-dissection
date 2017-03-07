885b20111213592fe4603728bc96f22e6b8f5464
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:22:41 +0100

    fixed files form Closure #46

--- projects/Closure/46/com/google/javascript/rhino/jstype/RecordType.java
+++ projects/Closure/46/com/google/javascript/rhino/jstype/RecordType.java
@@ -137,22 +137,6 @@ class RecordType extends PrototypeObjectType {
         propertyNode);
   }
 
-  @Override
-  public JSType getLeastSupertype(JSType that) {
-    if (!that.isRecordType()) {
-      return super.getLeastSupertype(that);
-    }
-    RecordTypeBuilder builder = new RecordTypeBuilder(registry);
-    for (String property : properties.keySet()) {
-      if (that.toMaybeRecordType().hasProperty(property) &&
-          that.toMaybeRecordType().getPropertyType(property).isEquivalentTo(
-              getPropertyType(property))) {
-        builder.addProperty(property, getPropertyType(property),
-            getPropertyNode(property));
-      }
-    }
-    return builder.build();
-  }
   JSType getGreatestSubtypeHelper(JSType that) {
     if (that.isRecordType()) {
       RecordType thatRecord = that.toMaybeRecordType();
