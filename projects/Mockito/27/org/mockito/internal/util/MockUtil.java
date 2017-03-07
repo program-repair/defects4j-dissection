95ae87cc0c924002d67a01fad36ad6a3672a381e
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:28:06 +0100

    fixed files form Mockito #27

--- projects/Mockito/27/org/mockito/internal/util/MockUtil.java
+++ projects/Mockito/27/org/mockito/internal/util/MockUtil.java
@@ -61,8 +61,7 @@ public class MockUtil {
 
     public <T> void resetMock(T mock) {
         MockHandlerInterface<T> oldMockHandler = getMockHandler(mock);
-        MockHandler<T> newMockHandler = new MockHandler<T>(oldMockHandler);
-        MethodInterceptorFilter newFilter = new MethodInterceptorFilter(newMockHandler, (MockSettingsImpl) org.mockito.Mockito.withSettings().defaultAnswer(org.mockito.Mockito.RETURNS_DEFAULTS));
+        MethodInterceptorFilter newFilter = newMethodInterceptorFilter(oldMockHandler.getMockSettings());
         ((Factory) mock).setCallback(0, newFilter);
     }
 
