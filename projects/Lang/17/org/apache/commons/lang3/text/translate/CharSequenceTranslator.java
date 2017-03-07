ce529bac6b3614c96512e04ae7f7d09bccefe981
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:25:59 +0100

    fixed files form Lang #17

--- projects/Lang/17/org/apache/commons/lang3/text/translate/CharSequenceTranslator.java
+++ projects/Lang/17/org/apache/commons/lang3/text/translate/CharSequenceTranslator.java
@@ -80,26 +80,20 @@ public abstract class CharSequenceTranslator {
             return;
         }
         int pos = 0;
-        int len = Character.codePointCount(input, 0, input.length());
+        int len = input.length();
         while (pos < len) {
             int consumed = translate(input, pos, out);
             if (consumed == 0) {
                 char[] c = Character.toChars(Character.codePointAt(input, pos));
                 out.write(c);
+                pos+= c.length;
+                continue;
             }
-            else {
 //          // contract with translators is that they have to understand codepoints 
 //          // and they just took care of a surrogate pair
             for (int pt = 0; pt < consumed; pt++) {
-                    if (pos < len - 2) {
                 pos += Character.charCount(Character.codePointAt(input, pos));
-                    } else {
-                        pos++;
-                    }
-                }
-                pos--;
             }
-            pos++;
         }
     }
 
