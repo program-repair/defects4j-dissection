1ef23c85f7688088679b33cc7f261bf0a7ed8d31
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:17:33 +0100

    fixed files form Math #60

--- projects/Math/60/org/apache/commons/math/distribution/NormalDistributionImpl.java
+++ projects/Math/60/org/apache/commons/math/distribution/NormalDistributionImpl.java
@@ -123,18 +123,11 @@ public class NormalDistributionImpl extends AbstractContinuousDistribution
      */
     public double cumulativeProbability(double x) throws MathException {
         final double dev = x - mean;
-        try {
+        if (FastMath.abs(dev) > 40 * standardDeviation) { 
+            return dev < 0 ? 0.0d : 1.0d;
+        }
         return 0.5 * (1.0 + Erf.erf((dev) /
                     (standardDeviation * FastMath.sqrt(2.0))));
-        } catch (MaxIterationsExceededException ex) {
-            if (x < (mean - 20 * standardDeviation)) { // JDK 1.5 blows at 38
-                return 0;
-            } else if (x > (mean + 20 * standardDeviation)) {
-                return 1;
-            } else {
-                throw ex;
-            }
-        }
     }
 
     /**
