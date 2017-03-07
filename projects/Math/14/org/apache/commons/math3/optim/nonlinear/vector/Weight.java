07edc747ef5c452dfeecc145e2bf070b0c80d4ed
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:54 +0100

    fixed files form Math #14

--- projects/Math/14/org/apache/commons/math3/optim/nonlinear/vector/Weight.java
+++ projects/Math/14/org/apache/commons/math3/optim/nonlinear/vector/Weight.java
@@ -40,10 +40,7 @@ public class Weight implements OptimizationData {
      */
     public Weight(double[] weight) {
         final int dim = weight.length;
-        weightMatrix = org.apache.commons.math3.linear.MatrixUtils.createRealMatrix(dim, dim);
-        for (int i = 0; i < dim; i++) {
-            weightMatrix.setEntry(i, i, weight[i]);
-        }
+        weightMatrix = new DiagonalMatrix(weight);
     }
 
     /**
