434b3eac9a30f2a72ac941cc9e737a136dfc5490
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:22:58 +0100

    fixed files form Closure #56

--- projects/Closure/56/com/google/javascript/jscomp/SourceFile.java
+++ projects/Closure/56/com/google/javascript/jscomp/SourceFile.java
@@ -239,7 +239,11 @@ public class SourceFile implements StaticSourceFile, Serializable {
       // If next new line cannot be found, there are two cases
       // 1. pos already reaches the end of file, then null should be returned
       // 2. otherwise, return the contents between pos and the end of file.
+      if (pos >= js.length()) {
         return null;
+      } else {
+        return js.substring(pos, js.length());
+      }
     } else {
       return js.substring(pos, js.indexOf('\n', pos));
     }
