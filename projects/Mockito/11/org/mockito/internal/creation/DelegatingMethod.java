487e8ea71b66d253aa364389bda0bc953ebfa3ba
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:27:40 +0100

    fixed files form Mockito #11

--- projects/Mockito/11/org/mockito/internal/creation/DelegatingMethod.java
+++ projects/Mockito/11/org/mockito/internal/creation/DelegatingMethod.java
@@ -52,11 +52,19 @@ public class DelegatingMethod implements MockitoMethod {
      */
     @Override
     public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof DelegatingMethod) {
+            DelegatingMethod that = (DelegatingMethod) o;
+            return method.equals(that.method);
+        } else {
             return method.equals(o);
+        }
     }
 
     @Override
     public int hashCode() {
-        return 1;
+        return method.hashCode();
     }
 }
