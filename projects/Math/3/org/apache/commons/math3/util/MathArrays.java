416c77d0f24f899e213e870b0b3e507a98e84ec6
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:26 +0100

    fixed files form Math #3

--- projects/Math/3/org/apache/commons/math3/util/MathArrays.java
+++ projects/Math/3/org/apache/commons/math3/util/MathArrays.java
@@ -818,7 +818,10 @@ public class MathArrays {
             throw new DimensionMismatchException(len, b.length);
         }
 
+        if (len == 1) {
             // Revert to scalar multiplication.
+            return a[0] * b[0];
+        }
 
         final double[] prodHigh = new double[len];
         double prodLowSum = 0;
