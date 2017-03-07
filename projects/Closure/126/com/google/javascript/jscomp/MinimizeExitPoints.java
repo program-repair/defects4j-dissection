ee70e2400f853a03e9dd0bc3f6fd3c10f2975856
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:25:17 +0100

    fixed files form Closure #126

--- projects/Closure/126/com/google/javascript/jscomp/MinimizeExitPoints.java
+++ projects/Closure/126/com/google/javascript/jscomp/MinimizeExitPoints.java
@@ -138,10 +138,6 @@ class MinimizeExitPoints
        * can cause problems if it changes the completion type of the finally
        * block. See ECMA 262 Sections 8.9 & 12.14
        */
-      if (NodeUtil.hasFinally(n)) {
-        Node finallyBlock = n.getLastChild();
-        tryMinimizeExits(finallyBlock, exitType, labelName);
-      }
     }
 
     // Just a 'label'.
