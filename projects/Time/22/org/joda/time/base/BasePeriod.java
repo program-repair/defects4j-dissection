a1707ea85a5657e5b2f63ac55b0b221c101e63f6
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:12 +0100

    fixed files form Time #22

--- projects/Time/22/org/joda/time/base/BasePeriod.java
+++ projects/Time/22/org/joda/time/base/BasePeriod.java
@@ -219,8 +219,13 @@ public abstract class BasePeriod
      * @param duration  the duration, in milliseconds
      */
     protected BasePeriod(long duration) {
-        this(duration, null, null);
+        super();
         // bug [3264409]
+        iType = PeriodType.time();
+        int[] values = ISOChronology.getInstanceUTC().get(this, duration);
+        iType = PeriodType.standard();
+        iValues = new int[8];
+        System.arraycopy(values, 0, iValues, 4, 4);
     }
 
     /**
