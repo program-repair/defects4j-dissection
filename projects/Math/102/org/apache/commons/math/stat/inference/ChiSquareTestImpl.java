2b1f16c84cd18021302baed1286ac420a18d9a1d
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:19:48 +0100

    fixed files form Math #102

--- projects/Math/102/org/apache/commons/math/stat/inference/ChiSquareTestImpl.java
+++ projects/Math/102/org/apache/commons/math/stat/inference/ChiSquareTestImpl.java
@@ -71,11 +71,28 @@ public class ChiSquareTestImpl implements UnknownDistributionChiSquareTest {
             throw new IllegalArgumentException(
                 "observed counts must be non-negative and expected counts must be postive");
         }
+        double sumExpected = 0d;
+        double sumObserved = 0d;
+        for (int i = 0; i < observed.length; i++) {
+            sumExpected += expected[i];
+            sumObserved += observed[i];
+        }
+        double ratio = 1.0d;
+        boolean rescale = false;
+        if (Math.abs(sumExpected - sumObserved) > 10E-6) {
+            ratio = sumObserved / sumExpected;
+            rescale = true;
+        }
         double sumSq = 0.0d;
         double dev = 0.0d;
         for (int i = 0; i < observed.length; i++) {
+            if (rescale) {
+                dev = ((double) observed[i] - ratio * expected[i]);
+                sumSq += dev * dev / (ratio * expected[i]);
+            } else {
                 dev = ((double) observed[i] - expected[i]);
                 sumSq += dev * dev / expected[i];
+            }
         }
         return sumSq;
     }
