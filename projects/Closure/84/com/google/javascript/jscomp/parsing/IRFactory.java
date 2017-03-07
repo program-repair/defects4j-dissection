b6445461226920f77aa719f35a55f69aba6ceb86
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:23:58 +0100

    fixed files form Closure #84

--- projects/Closure/84/com/google/javascript/jscomp/parsing/IRFactory.java
+++ projects/Closure/84/com/google/javascript/jscomp/parsing/IRFactory.java
@@ -338,6 +338,13 @@ public class IRFactory {
     @Override
     Node processAssignment(Assignment assignmentNode) {
       Node assign = processInfixExpression(assignmentNode);
+      Node target = assign.getFirstChild();
+      if (!validAssignmentTarget(target)) {
+        errorReporter.error(
+          "invalid assignment target",
+          sourceName,
+          target.getLineno(), "", 0);
+      }
       return assign;
     }
 
@@ -794,6 +801,17 @@ public class IRFactory {
         operand.setDouble(-operand.getDouble());
         return operand;
       } else {
+        if (type == Token.INC || type == Token.DEC) {
+          if (!validAssignmentTarget(operand)) {
+            String msg = (type == Token.INC)
+                ? "invalid increment target"
+                : "invalid decrement target";
+            errorReporter.error(
+              msg,
+              sourceName,
+              operand.getLineno(), "", 0);
+          }
+        }
 
         Node node = newNode(type, operand);
         if (exprNode.isPostfix()) {
@@ -803,6 +821,15 @@ public class IRFactory {
       }
     }
 
+    private boolean validAssignmentTarget(Node target) {
+      switch (target.getType()) {
+        case Token.NAME:
+        case Token.GETPROP:
+        case Token.GETELEM:
+          return true;
+      }
+      return false;
+    }
 
     @Override
     Node processVariableDeclaration(VariableDeclaration declarationNode) {
