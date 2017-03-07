7ff6b0915ca239fbcdd6a98077ecd732cd7c6657
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:19:10 +0100

    fixed files form Math #89

--- projects/Math/89/org/apache/commons/math/stat/Frequency.java
+++ projects/Math/89/org/apache/commons/math/stat/Frequency.java
@@ -107,7 +107,11 @@ public class Frequency implements Serializable {
      */
     @Deprecated
     public void addValue(Object v) {
+        if (v instanceof Comparable<?>){
             addValue((Comparable<?>) v);            
+        } else {
+            throw new IllegalArgumentException("Object must implement Comparable");
+        }
     }
     
     /**
