c19847efedabd5ff38a2c6c4f79f622450a9eb01
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:21 +0100

    fixed files form Math #1

--- projects/Math/1/org/apache/commons/math3/fraction/BigFraction.java
+++ projects/Math/1/org/apache/commons/math3/fraction/BigFraction.java
@@ -303,6 +303,9 @@ public class BigFraction
             if ((p2 > overflow) || (q2 > overflow)) {
                 // in maxDenominator mode, if the last fraction was very close to the actual value
                 // q2 may overflow in the next iteration; in this case return the last one.
+                if (epsilon == 0.0 && FastMath.abs(q1) < maxDenominator) {
+                    break;
+                }
                 throw new FractionConversionException(value, p2, q2);
             }
 
--- projects/Math/1/org/apache/commons/math3/fraction/Fraction.java
+++ projects/Math/1/org/apache/commons/math3/fraction/Fraction.java
@@ -212,6 +212,9 @@ public class Fraction
             if ((FastMath.abs(p2) > overflow) || (FastMath.abs(q2) > overflow)) {
                 // in maxDenominator mode, if the last fraction was very close to the actual value
                 // q2 may overflow in the next iteration; in this case return the last one.
+                if (epsilon == 0.0 && FastMath.abs(q1) < maxDenominator) {
+                    break;
+                }
                 throw new FractionConversionException(value, p2, q2);
             }
 
