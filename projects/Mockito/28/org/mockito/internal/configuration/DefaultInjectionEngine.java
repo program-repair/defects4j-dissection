f8d9803f928aa793b534d37670bb089eee1a442e
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:28:07 +0100

    fixed files form Mockito #28

--- projects/Mockito/28/org/mockito/internal/configuration/DefaultInjectionEngine.java
+++ projects/Mockito/28/org/mockito/internal/configuration/DefaultInjectionEngine.java
@@ -90,7 +90,8 @@ public class DefaultInjectionEngine {
 
     private void injectMockCandidate(Class<?> awaitingInjectionClazz, Set<Object> mocks, Object fieldInstance) {
         for(Field field : orderedInstanceFieldsFrom(awaitingInjectionClazz)) {
-            mockCandidateFilter.filterCandidate(mocks, field, fieldInstance).thenInject();
+            Object injected = mockCandidateFilter.filterCandidate(mocks, field, fieldInstance).thenInject();
+            mocks.remove(injected);
         }
     }
 
