9704eaf88d3c7caf1271d902b76d060b8bbad173
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:25:12 +0100

    fixed files form Closure #123

--- projects/Closure/123/com/google/javascript/jscomp/CodeGenerator.java
+++ projects/Closure/123/com/google/javascript/jscomp/CodeGenerator.java
@@ -282,7 +282,7 @@ class CodeGenerator {
       case Token.HOOK: {
         Preconditions.checkState(childCount == 3);
         int p = NodeUtil.precedence(type);
-        Context rhsContext = Context.OTHER;
+        Context rhsContext = getContextForNoInOperator(context);
         addExpr(first, p + 1, context);
         cc.addOp("?", true);
         addExpr(first.getNext(), 1, rhsContext);
