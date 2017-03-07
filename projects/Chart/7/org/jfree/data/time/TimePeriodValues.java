8b8165c0bd683435e29d59463a4e216e31ed2fa7
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:20:15 +0100

    fixed files form Chart #7

--- projects/Chart/7/org/jfree/data/time/TimePeriodValues.java
+++ projects/Chart/7/org/jfree/data/time/TimePeriodValues.java
@@ -297,9 +297,9 @@ public class TimePeriodValues extends Series implements Serializable {
         }
         
         if (this.maxMiddleIndex >= 0) {
-            long s = getDataItem(this.minMiddleIndex).getPeriod().getStart()
+            long s = getDataItem(this.maxMiddleIndex).getPeriod().getStart()
                 .getTime();
-            long e = getDataItem(this.minMiddleIndex).getPeriod().getEnd()
+            long e = getDataItem(this.maxMiddleIndex).getPeriod().getEnd()
                 .getTime();
             long maxMiddle = s + (e - s) / 2;
             if (middle > maxMiddle) {
