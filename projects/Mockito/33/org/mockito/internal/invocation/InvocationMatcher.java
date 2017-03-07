c91ecafc98bf4794c2f3ae6a60a996cc388fd6b4
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:28:15 +0100

    fixed files form Mockito #33

--- projects/Mockito/33/org/mockito/internal/invocation/InvocationMatcher.java
+++ projects/Mockito/33/org/mockito/internal/invocation/InvocationMatcher.java
@@ -95,8 +95,19 @@ public class InvocationMatcher implements PrintableInvocation, PrintingFriendlyI
         Method m1 = invocation.getMethod();
         Method m2 = candidate.getMethod();
         
+        if (m1.getName() != null && m1.getName().equals(m2.getName())) {
         	/* Avoid unnecessary cloning */
-        return m1.equals(m2);
+        	Class[] params1 = m1.getParameterTypes();
+        	Class[] params2 = m2.getParameterTypes();
+        	if (params1.length == params2.length) {
+        	    for (int i = 0; i < params1.length; i++) {
+        		if (params1[i] != params2[i])
+        		    return false;
+        	    }
+        	    return true;
+        	}
+        }
+        return false;
     }
     
     public Location getLocation() {
