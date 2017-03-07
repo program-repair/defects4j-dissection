0994fb260162f610c02091635753334018b56d03
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:14:00 +0100

    fixed files form Time #15

--- projects/Time/15/org/joda/time/field/FieldUtils.java
+++ projects/Time/15/org/joda/time/field/FieldUtils.java
@@ -135,6 +135,9 @@ public class FieldUtils {
     public static long safeMultiply(long val1, int val2) {
         switch (val2) {
             case -1:
+                if (val1 == Long.MIN_VALUE) {
+                    throw new ArithmeticException("Multiplication overflows a long: " + val1 + " * " + val2);
+                }
                 return -val1;
             case 0:
                 return 0L;
