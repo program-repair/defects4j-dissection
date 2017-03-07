c38f924dea3a4a48673b8cb20fc896778d9b93ba
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:24:24 +0100

    fixed files form Closure #97

--- projects/Closure/97/com/google/javascript/jscomp/PeepholeFoldConstants.java
+++ projects/Closure/97/com/google/javascript/jscomp/PeepholeFoldConstants.java
@@ -695,7 +695,8 @@ public class PeepholeFoldConstants extends AbstractPeepholeOptimization {
           // JavaScript handles zero shifts on signed numbers differently than
           // Java as an Java int can not represent the unsigned 32-bit number
           // where JavaScript can so use a long here.
-          result = lvalInt >>> rvalInt;
+          long lvalLong = lvalInt & 0xffffffffL;
+          result = lvalLong >>> rvalInt;
           break;
         default:
           throw new AssertionError("Unknown shift operator: " +
