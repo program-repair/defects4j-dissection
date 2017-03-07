586687330f33163229cf30ffda70e48f60af21de
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:24:22 +0100

    fixed files form Closure #96

--- projects/Closure/96/com/google/javascript/jscomp/TypeCheck.java
+++ projects/Closure/96/com/google/javascript/jscomp/TypeCheck.java
@@ -1406,10 +1406,13 @@ public class TypeCheck implements NodeTraversal.Callback, CompilerPass {
     Node parameter = null;
     Node argument = null;
     while (arguments.hasNext() &&
-           parameters.hasNext()) {
+           (parameters.hasNext() ||
+            parameter != null && parameter.isVarArgs())) {
       // If there are no parameters left in the list, then the while loop
       // above implies that this must be a var_args function.
+      if (parameters.hasNext()) {
         parameter = parameters.next();
+      }
       argument = arguments.next();
       ordinal++;
 
