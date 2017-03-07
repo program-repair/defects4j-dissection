b4c2f43dfc6ed3a1285faf399e321a05c2035f3b
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:19:45 +0100

    fixed files form Math #101

--- projects/Math/101/org/apache/commons/math/complex/ComplexFormat.java
+++ projects/Math/101/org/apache/commons/math/complex/ComplexFormat.java
@@ -374,7 +374,8 @@ public class ComplexFormat extends Format implements Serializable {
         int n = getImaginaryCharacter().length();
         startIndex = pos.getIndex();
         int endIndex = startIndex + n;
-        if (
+        if ((startIndex >= source.length()) ||
+            (endIndex > source.length()) ||
             source.substring(startIndex, endIndex).compareTo(
             getImaginaryCharacter()) != 0) {
             // set index back to initial, error index should be the start index
