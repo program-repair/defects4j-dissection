9e99aab58052b2044f88677ad84232d26965f6b5
tdurieux <durieuxthomas@hotmail.com>
2017-03-07 13:21:19 +0100

    fixed files form Closure #10

--- projects/Closure/10/com/google/javascript/jscomp/NodeUtil.java
+++ projects/Closure/10/com/google/javascript/jscomp/NodeUtil.java
@@ -1414,7 +1414,7 @@ public final class NodeUtil {
 
   static boolean mayBeString(Node n, boolean recurse) {
     if (recurse) {
-      return allResultsMatch(n, MAY_BE_STRING_PREDICATE);
+      return anyResultsMatch(n, MAY_BE_STRING_PREDICATE);
     } else {
       return mayBeStringHelper(n);
     }
